d00d8f36b9fd9f846b9209dcfce302b5f60fb836
Harsh Tarkar <tarkar.harsh@gmail.com>
2024-08-24 23:18:04 +0530

    Adding Google Auth

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import env from "dotenv";
+import session from "express-session";
+import passport from "passport";
+import GoogleStrategy from "passport-google-oauth2";
 
 const app = express();
 const port = 3000;
@@ -18,6 +21,17 @@ db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    maxAge: 1000 * 60 * 60 * 24,
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
@@ -27,13 +41,20 @@ async function checkVisisted() {
   });
   return countries;
 }
-// GET home page
+
 app.get("/", async (req, res) => {
-  const countries = await checkVisisted();
-  res.render("index.ejs", { countries: countries, total: countries.length });
+  if(req.isAuthenticated()){
+    const countries = await checkVisisted();
+    res.render("index.ejs", { countries: countries, total: countries.length });
+  }else{
+    res.redirect("/login");
+  }
 });
 
-//INSERT new country
+app.get("/login", async (req,res) => {
+  res.render("login.ejs");
+})
+
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
@@ -70,6 +91,50 @@ app.post("/add", async (req, res) => {
     });
   }
 });
+
+app.post("/logout", (req,res) => {
+  req.logout((err) => {
+    if (err) console.log(err);
+    res.redirect("/");
+  })
+})
+
+app.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"],
+}));
+
+app.get("/auth/google/secrets", passport.authenticate("google", {
+  successRedirect: "/",
+  failureRedirect: "/login"
+}));
+
+passport.use("google", new GoogleStrategy({
+  clientID: process.env.G_CLIENT_ID,
+  clientSecret: process.env.G_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+}, async (accessToken, refreshToken, profile, cb) => {
+  console.log(profile);
+  try{
+    const result = await db.query("select * from users where email = $1", [profile.email]);
+    if(result.rows.length === 0){
+      const newUser = await db.query("insert into users (email, password) values($1,$2)",[profile.email,"google"]);
+      cb(null, newUser.rows[0]);
+    }else{
+      cb(null, result.rows[0]);
+    }
+  }catch(err){
+    cb(err);
+  }
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+})
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
